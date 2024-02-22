ca3dda6fccd24034fa98eb8062c805f0efc7cb56
dawwson <shines1427@gmail.com>
2024-02-23 01:17:16 +0900

    Comment: JSDoc 적용

--- src/myPackage.js
+++ src/myPackage.js
@@ -1,7 +1,19 @@
+// @ts-check
+/**
+ * Initialize the project
+ * @param {object} config
+ * @param {string} config.url
+ * @returns {boolean}
+ */
 export function init(config) {
   return true;
 }
 
+/**
+ * Exits then program
+ * @param {number} code
+ * @returns {number}
+ */
 export function exit(code) {
   return code + 1;
 }
